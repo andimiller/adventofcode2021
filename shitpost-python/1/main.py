6ad4a354e8d8446abb00f72324b0966bae754973
Andi Miller <andi@andimiller.net>
2021-12-01 19:27:20 +0000

    add day 1 part 2

--- shitpost-python/1/main.py
+++ shitpost-python/1/main.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 from collections.abc import Sequence, Callable, Iterable
+from collections import deque
 from typing import TypeVar, Generic
-from itertools import pairwise, starmap
+from itertools import pairwise, starmap, islice
 import operator
 
 
@@ -14,6 +15,19 @@ def tupled(f: Callable[[T, T2], O]) -> Callable[[Tuple[T, T2]], O]:
     return lambda t: f(t[0], t[1])
 
 
+def slider(input: Iterable[T], window: int) -> Iterable[list[T]]:
+    i = iter(input)
+    queue = deque(islice(i, window - 1), window)
+    for item in i:
+        queue.append(item)
+        yield list(queue)
+
+
+def _tap(f: Callable[T, T2], x: T) -> T:
+    f(x)
+    return x
+
+
 class Stream(Generic[T]):
     underlying: Iterable[T]
 
@@ -22,6 +36,9 @@ class Stream(Generic[T]):
 
     def map(self, f: Callable[[T], T2]) -> Stream[T2]:
         return Stream(map(f, self.underlying))
+
+    def tap(self, f: Callable[[T], T2]) -> Stream[T]:
+        return Stream(map(lambda x: _tap(f, x), self.underlying))
 
     def toIterator(self) -> Iterable[T]:
         return self.underlying
@@ -35,6 +52,11 @@ class Stream(Generic[T]):
     def runToList(self) -> List[T]:
         return list(self.underlying)
 
+    # methods for part 2
+
+    def sliding(self, window: int) -> Stream[list[t]]:
+        return Stream(slider(self.underlying, window))
+
 
 def identity(t: T) -> T:
     return t
@@ -42,9 +64,17 @@ def identity(t: T) -> T:
 
 if __name__ == "__main__":
     with open("input.txt") as fh:
+        print("part 1")
+        r = Stream(fh).map(int).zipWithNext().runCount(tupled(operator.lt))
+        print(r)
+
+    with open("input.txt") as fh:
+        print("part 2")
         r = (
             Stream(fh)
             .map(int)
+            .sliding(3)
+            .map(sum)
             .zipWithNext()
             .runCount(tupled(operator.lt))
         )
